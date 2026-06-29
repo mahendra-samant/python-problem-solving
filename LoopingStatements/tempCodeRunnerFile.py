7449be42265851dc03fc570eb5df3c966f0be58d
dutondcoder <mailingmahendra@gmail.com>
2026-06-29 14:32:11 +0530

    Remove VS Code temporary file

--- LoopingStatements/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-otp=int(input("Enter the OTP :"))
-originalOTP=123456
-
-while otp!= originalOTP:
-    print("Please enter the valid OTP!!")
-    otp=int(input("Enter the OTP :"))
-
-print("Login to phonePe successfully!!")  
